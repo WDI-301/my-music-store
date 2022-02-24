c8eb14ad6c03103544be8dd13eba16b88e14c6fa
Omar Suriel <omarsuriel@icloud.com>
2022-02-24 16:07:46 -0500

    local storage to persist state

--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -4,10 +4,19 @@ import { userReducer } from './userState';
 
 
 const store = configureStore({
+  preloadedState: JSON.parse(window.localStorage.getItem('applicationState')) || {},
   reducer: {
     shoppingCart: shoppingCartReducer,
     user: userReducer,
   },
 })
+
+const handleChange = () => {
+  const newState = store.getState();
+
+  window.localStorage.setItem('applicationState', JSON.stringify(newState));
+}
+
+store.subscribe(handleChange)
 
 export default store;
